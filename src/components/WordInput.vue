8489f65d33c90963b304e9a61ff6f8a65a9b3294
Steven Ijames <ijamesproductions1@gmail.com>
2021-10-24 19:23:44 -0600

    created axios method in WordInput component

--- src/components/WordInput.vue
+++ src/components/WordInput.vue
@@ -1,20 +1,46 @@
 <template>
   <div>
-    <label for="word">{{ message }}</label>
+    <label for="word">Search for Definition</label>
 
-    <input type="search" id="word" name="word" />
-    <button>search</button>
+    <input
+      v-model="typedWord"
+      type="search"
+      id="word"
+      name="word"
+      placeholder="Type Word Here"
+    />
+    <p>Definition for Word: {{ typedWord }}</p>
+    <button @click="getDefinition">search</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "WordInput",
   data() {
     return {
-      typedWord: "abc",
-      message: "Type your word here",
+      typedWord: "",
+      wordDefinition: [],
     };
   },
+  methods: {
+    async getDefinition() {
+      try {
+        const response = await axios.get(
+          `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${this.typedWord}?key=dde18990-3467-4b26-bb9f-c8217de8b929`
+        );
+        this.wordDefinition = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
+// User Types word in search bar
+// Press Search
+// Make a request https://www.dictionaryapi.com/api/v3/references/collegiate/json/joyful?key=dde18990-3467-4b26-bb9f-c8217de8b929
+
+// Now that we have the data what do we want to do with it: A. Display it and styled B. Data Properties Displayed
 </script>
